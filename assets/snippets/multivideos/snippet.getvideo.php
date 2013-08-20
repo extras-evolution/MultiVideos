4d98b3cac5a5e7ba9a983f16c4fd2b2f7867d14a
Pathologic <maxx@np.by>
2013-08-21 00:12:25 +0300

    Release v.1.0

---
+++ assets/snippets/multivideos/snippet.getvideo.php
@@ -0,0 +1,20 @@
+<?php
+if(!defined('MODX_BASE_PATH')){die('What are you doing? Get out of here!');}
+$output = '';
+if (isset($link)) {
+$thumbsUrl = isset($thumbsUrl) ? $thumbsUrl : 'assets/images/video/'; //thumbs folder
+$emptyImage = isset($emptyImage) ? $emptyImage : 'assets/snippets/phpthumb/noimage.png'; //return this image if there's no image to return
+$action = isset($action) ? $action : 'embed';
+if (!class_exists('videoThumb'))include_once(MODX_BASE_PATH.'assets/plugins/multi/videothumb.class.php');
+$video = new videoThumb(array(
+	'imagesPath' => MODX_BASE_PATH . '/' . $thumbsUrl
+	,'imagesUrl' => $thumbsUrl
+	,'emptyImage' => $emptyImage
+));
+switch ($action) {
+	case 'embed' : $embed = $video->process($link,false); $output = $embed['video']; break;
+	case 'thumb' : $thumb = $video->process($link,true); $output = $thumb['image']; break;
+}
+}
+return $output;
+?>
